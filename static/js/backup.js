87724eebe90a51616f297c5baf59e0e0fb172cba
OctFog <xix063@ucsd.edu>
2025-04-16 19:52:29 -0700

    Copid everything from my branch

---
+++ static/js/backup.js
@@ -0,0 +1,23 @@
+// const shuffleDeck = function() {
+//     const values = Array.from({ length: 10 }, (_, i) => `Card ${i + 1}`);
+//     for (let i = values.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         console.log(values[i], values[j]);
+//         console.log(i, j);
+
+//         [values[i], values[j]] = [values[j], values[i]];
+//     }
+//     print(values);
+//     return values;
+// }
+// const shuffleAndRedeal = function() {
+//     // cards = shuffleDeck();
+//     if (shuffled === 0) {
+//         shuffled = shuffleDeck();
+//     } else {
+//         clearDesk();
+//         shuffled = 0;
+//     }
+
+ 
+// }
